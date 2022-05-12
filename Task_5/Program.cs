586d0821e015cffb16163cc9548346082bb0f170
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-12 20:27:41 +0300

    #1

---
+++ Task_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Daniil";
+            string lastName = "Gorin";
+            string temporaryName;
+
+            Console.WriteLine(firstName + " " + lastName);
+
+            temporaryName = firstName;
+            firstName = lastName;
+            lastName = temporaryName;
+
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
